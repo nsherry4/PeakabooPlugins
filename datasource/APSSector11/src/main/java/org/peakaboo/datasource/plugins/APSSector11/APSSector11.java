b293a7f2dfa515005dbd8aa958f1b4f44516f944
Nathaniel Sherry <nsherry4@uwo.ca>
2019-12-02 11:26:18 -0500

    Added APS Sector 11 to repo

---
+++ datasource/APSSector11/src/main/java/org/peakaboo/datasource/plugins/APSSector11/APSSector11.java
@@ -0,0 +1,26 @@
+package org.peakaboo.datasource.plugins.APSSector11;
+
+import org.peakaboo.datasource.plugins.GenericHDF5.FloatMatrixHDF5DataSource;
+
+public class APSSector11 extends FloatMatrixHDF5DataSource  {
+
+	private static final String PATH = "/data/data";
+	private static final String NAME = "APS-11 Format";
+	private static final String DESC = "APS-11 XRF spectral data stored in an HDF5 container";
+	
+	public APSSector11() {
+		super("yxz", PATH, NAME, DESC);
+	}
+
+	@Override
+	public String pluginVersion() {
+		return "0.1";
+	}
+
+	@Override
+	public String pluginUUID() {
+		return "37b76d2b-7cab-4b9f-9609-e4e9aba071bd";
+	}
+	
+
+}
