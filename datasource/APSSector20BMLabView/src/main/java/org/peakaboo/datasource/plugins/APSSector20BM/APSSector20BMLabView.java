c6596e0f16a196d0331fb219d293c3ba18df1c3e
Nathaniel Sherry <nsherry4@uwo.ca>
2020-01-14 16:00:29 -0500

    Added APS Sector 20-BM LabView datasource plugin

---
+++ datasource/APSSector20BMLabView/src/main/java/org/peakaboo/datasource/plugins/APSSector20BM/APSSector20BMLabView.java
@@ -0,0 +1,130 @@
+package org.peakaboo.datasource.plugins.APSSector20BM;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.peakaboo.datasource.model.components.datasize.DataSize;
+import org.peakaboo.datasource.model.components.fileformat.FileFormat;
+import org.peakaboo.datasource.model.components.fileformat.SimpleFileFormat;
+import org.peakaboo.datasource.model.components.metadata.Metadata;
+import org.peakaboo.datasource.model.components.physicalsize.PhysicalSize;
+import org.peakaboo.datasource.model.components.scandata.ScanData;
+import org.peakaboo.datasource.model.components.scandata.SimpleScanData;
+import org.peakaboo.datasource.model.components.scandata.analysis.Analysis;
+import org.peakaboo.datasource.model.components.scandata.analysis.DataSourceAnalysis;
+import org.peakaboo.datasource.plugin.AbstractDataSource;
+import org.peakaboo.framework.autodialog.model.Group;
+import org.peakaboo.framework.cyclops.ISpectrum;
+import org.peakaboo.framework.cyclops.ReadOnlySpectrum;
+import org.peakaboo.framework.cyclops.Spectrum;
+import org.peakaboo.framework.cyclops.util.StringInput;
+
+
+
+public class APSSector20BMLabView extends AbstractDataSource {
+
+	private SimpleScanData scandata;
+	
+	@Override
+	public Optional<Group> getParameters(List<Path> paths) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Metadata> getMetadata() {
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<DataSize> getDataSize() {
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<PhysicalSize> getPhysicalSize() {
+		return Optional.empty();
+	}
+
+	@Override
+	public FileFormat getFileFormat() {
+		return new APSSector20BMLabViewFileFormat();
+	}
+
+	@Override
+	public ScanData getScanData() {
+		return scandata;
+	}
+
+	@Override
+	public void read(List<Path> paths) throws Exception {
+		if (paths.size() != 1) {
+			throw new IllegalArgumentException("Expected exactly 1 file");
+		}
+		
+		Path path = paths.get(0);
+		
+		scandata = new SimpleScanData(path.getFileName().toString());
+		
+		try (InputStream in = Files.newInputStream(path)) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			//search for the start of the data
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) { return; }
+				if (line.trim().toLowerCase().startsWith("# column headings:")) {
+					//found the start of the data
+					break;
+				}
+			}
+			
+			//clear the "#channel det 1 det 2" header line
+			reader.readLine();
+			
+			List<Float> channels = new ArrayList<>();
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) { break; }
+				line = line.trim();
+				if (line.startsWith("#")) { continue; }
+				String[] parts = line.split("\\s+");
+				int channelNumber = Integer.parseInt(parts[0]);
+				if (channelNumber != channels.size()) {
+					throw new RuntimeException("Channels must be listed in order");
+				}
+				float sum = 0f;
+				for (String part : Arrays.copyOfRange(parts, 1, parts.length)) {
+					sum += Float.parseFloat(part);
+				}
+				sum /= (float)(parts.length-1);
+				channels.add(sum);
+			}
+			
+			scandata.add(new ISpectrum(channels));
+			
+		}
+		
+	}
+
+	@Override
+	public String pluginVersion() {
+		return "0.1";
+	}
+
+	@Override
+	public String pluginUUID() {
+		return "675b80f4-c6b3-4e5f-89e5-ac1c01dec86d";
+	}
+	
+}
---
+++ datasource/APSSector20BMLabView/src/main/java/org/peakaboo/datasource/plugins/APSSector20BM/APSSector20BMLabViewFileFormat.java
@@ -0,0 +1,72 @@
+package org.peakaboo.datasource.plugins.APSSector20BM;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.peakaboo.common.PeakabooLog;
+import org.peakaboo.datasource.model.components.fileformat.FileFormat;
+import org.peakaboo.datasource.model.components.fileformat.FileFormatCompatibility;
+
+public class APSSector20BMLabViewFileFormat implements FileFormat {
+
+	@Override
+	public List<String> getFileExtensions() {
+		return Arrays.asList("0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009");
+	}
+
+	@Override
+	public FileFormatCompatibility compatibility(List<Path> filenames) {
+		if (filenames.size() != 1) {
+			return FileFormatCompatibility.NO;
+		}
+		
+		Path file = filenames.get(0);
+		try (InputStream in = Files.newInputStream(file)){
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			
+			String line;
+			while (true) {
+				line = reader.readLine();
+				if (line == null) {
+					//whoopsie
+					return FileFormatCompatibility.NO;
+				}
+				if (!line.trim().isEmpty()) {
+					//found the first non-empty line
+					break;
+				}
+			}
+			
+			
+			if (line.trim().toLowerCase().startsWith("#1-d data file")) {
+				return FileFormatCompatibility.YES_BY_CONTENTS;
+			} else {
+				return FileFormatCompatibility.NO;
+			}
+			
+		} catch (IOException e) {
+			PeakabooLog.get().log(Level.WARNING, "Failed to inspect file(s)", e);
+			return FileFormatCompatibility.NO;
+		}
+		
+	}
+
+	@Override
+	public String getFormatName() {
+		return "APS Sector 20-BM LabVIEW";
+	}
+
+	@Override
+	public String getFormatDescription() {
+		return "LabVIEW Spot Data from Sector 20-BM of the APS Synchrotron";
+	}
+
+
+}
